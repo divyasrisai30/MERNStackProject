ad9877ddad122e48f7a7cef6af1ab3a9704620b9
Dbojanki <dbojanki@asu.edu>
2024-05-20 21:02:38 -0700

    creating User listing page using UserList, UserItem, User and Shared folder

---
+++ app/src/User/Components/UserItem.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Avatar from '../../Shared/Componets/UIElements/Avatar'
+import Card from '../../Shared/Componets/UIElements/Card'
+
+import "./UserItem.css"
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+// import { Card } from "../../Shared/Componets/UIElements/Card"
+
+
+export default function UserItem(props) {
+  return (
+   <li className='user-item'>
+    <div className='user-item__content'>
+        <Card>
+            <Link to="/{`/${props.id}/places`}">
+            <div className='user-item__image'>
+                    <Avatar src={props.image} ait={props.name} ></Avatar>
+                    {/* <img src={props.image} alt={props.name}/> */}
+                </div>
+                <div className='user-item__info'>
+                    <h2>{props.name}</h2>
+                    <h3>
+                        {props.placeCount}{props.placeCount === 1 ? 'Place': "Places"}
+                    </h3>
+                </div>
+            </Link>
+        </Card>
+    </div>
+    
+   </li>
+  )
+}
--- app/src/User/Pages/Users.js
+++ app/src/User/Pages/Users.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import UserList from '../Components/UserList'
 
 function Users() {
+  const USER=[
+    {
+    id:'u1',
+    image:"http://thewowstyle.com/wp-content/uploads/2015/01/nature-images.jpg",
+    name:"Max Schwarz",
+    places:"3"
+  }
+]
   return (
-    <div>Hello World</div>
+    <UserList items={USER}/>
   )
 }
 
