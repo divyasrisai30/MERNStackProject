a3c041a3b210e8ca52e31cf7f14b4d47de4e4ec4
Dbojanki <divyasrisai30@gmail.com>
2024-05-25 19:04:22 -0700

    Working of 'View in map' button working without display of map

--- app/src/Places/Components/PlaceItem.js
+++ app/src/Places/Components/PlaceItem.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Card from "../../Shared/Componets/UIElements/Card";
 import Button from "../../Shared/FormElements/Button";
 
 import "./PlaceItem.css"
+import Modal from "../../Shared/Componets/UIElements/Modal";
 
 const PlaceItem = props =>{
+    const [showMap, setShowMap] = useState(false);
+
+    const openMapHeandler = ()=> setShowMap(true);
+
+    const closeMapHeandler = ()=> setShowMap(false);
     return(
-        
+        <React.Fragment>
+            <Modal show={showMap} 
+                onCancel={closeMapHeandler} 
+                header={props.address} 
+                contentClass="place-item__-content"
+                footerClass="place-item__-actions"
+                footer={<Button onClick={closeMapHeandler}>CLOSE</Button>}>
+                    <div className="map-container">
+                        <h2>Map</h2>
+                    </div>
+                </Modal>
         <li className="place-item">
             <Card className="place-item__content">
                 <div className="place-item__image">
@@ -19,12 +35,13 @@ const PlaceItem = props =>{
                     <p>{props.description}</p>
                 </div>
                 <div className="place-item__actions">
-                    <Button inverse>VIEW IN MAP</Button>
+                    <Button inverse onClick={openMapHeandler}>VIEW IN MAP</Button>
                     <Button to={`/places/${props.id}`}>EDIT</Button>
                     <Button danger>DELETE</Button>
                 </div>
             </Card>
         </li>
+        </React.Fragment>
     )
     
     
