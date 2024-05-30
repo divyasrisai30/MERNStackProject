3d0f9b93756af0286d3c6832e72fa9f078b71edd
Dbojanki <divyasrisai30@gmail.com>
2024-05-29 17:41:09 -0700

    Usage of UseRender instead of two useSates that has interdependance

--- app/src/Places/Pages/NewPlaces.js
+++ app/src/Places/Pages/NewPlaces.js
@@ -8,7 +8,7 @@ import "./NewPlaces.css"
 export default function NewPlaces() {
   return (
     <form className='place-form'>
-      <Input element="input" type="text" label="Title" vaildator={[]}></Input>
+      <Input element="input" type="text" label="Title" vaildator={[]} errorText="Please enter a valid title. "></Input>
     </form>
   )
 }
